31aa8c172b7f11670ccf1f5c1674e58a1ee4d2d2
Carlos Marte <carlosmarte23@gmail.com>
2020-12-18 17:11:15 -0400

    Setup of firebase context and config

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './App';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+  apiKey: 'AIzaSyAdQf19lu5I8h0lLupeCAmL6GQARwq-tr4',
+  authDomain: 'netflix-react-clone-aea22.firebaseapp.com',
+  projectId: 'netflix-react-clone-aea22',
+  storageBucket: 'netflix-react-clone-aea22.appspot.com',
+  messagingSenderId: '414793598771',
+  appId: '1:414793598771:web:946633717ee109190a6046',
+};
+
+// const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById('root')
 );
