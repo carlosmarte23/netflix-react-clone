aa855c5f6c0ddee910ceb671320b7a3f0db8aba3
Carlos Marte <carlosmarte23@gmail.com>
2020-12-21 13:09:55 -0400

    Adding heade component to browse container

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -33,6 +33,18 @@ export const Container = styled.div`
   }
 `;
 
+export const Group = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Text = styled.p`
+  color: white;
+  font-size: 22px;
+  line-height: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+`;
+
 export const Link = styled.div`
   color: #fff;
   text-decoration: none;
