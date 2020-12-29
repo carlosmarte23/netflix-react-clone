31ea80fb040481833e95f6d9bf98d159f35cc98f
Carlos Marte <carlosmarte23@gmail.com>
2020-12-29 19:26:27 -0400

    Modifying App to use protected routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
 import { Home, SignIn, SignUp, Browse } from './pages';
 import * as ROUTES from './constants/routes';
 import { useAuthListener } from './hooks';
+import { IsUserRedirect, ProtectedRoute } from './helper/routes';
 
 export function App() {
   const { user } = useAuthListener();
-
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Switch>
-        <Route path={ROUTES.SIGN_IN}>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
           <SignIn />
-        </Route>
-        <Route path={ROUTES.SIGN_UP}>
+        </IsUserRedirect>
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
           <SignUp />
-        </Route>
-        <Route path={ROUTES.BROWSE}>
+        </IsUserRedirect>
+
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
           <Browse />
-        </Route>
-        <Route path={ROUTES.HOME}>
+        </ProtectedRoute>
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
           <Home />
-        </Route>
+        </IsUserRedirect>
       </Switch>
     </Router>
   );
--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useContext } from 'react';
-import { useContent } from '.';
 
 import { FirebaseContext } from '../context/firebase';
 
