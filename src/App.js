5bca5c23eaa4d7c76654e878977c2dba86853cf6
Carlos Marte <carlosmarte23@gmail.com>
2020-12-29 18:47:12 -0400

    Custom Hook auth listener

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Home, SignIn, SignUp, Browse } from './pages';
 import * as ROUTES from './constants/routes';
+import { useAuthListener } from './hooks';
 
 export function App() {
+  const { user } = useAuthListener();
+
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Switch>
